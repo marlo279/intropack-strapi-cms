b4258b651510a9994b8b62c887a909fc43ee7380
Marlo Celestri <marlocelestri@casenine.com>
2026-04-10 16:28:20 +0200

    Added filter methode on split()

--- config/server.js
+++ config/server.js
@@ -2,7 +2,8 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env("PORT", "1337"),
   app: {
-    keys: env("APP_KEYS", "").split(","),
+    // keys: env("APP_KEYS", "").split(","),
+    keys: env("APP_KEYS", "").split(",").filter(Boolean),
   },
   webhooks: {
     populateRelations: env("WEBHOOKS_POPULATE_RELATIONS", "false") === "true",
